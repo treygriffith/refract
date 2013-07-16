1dcabe4e18ff862c9f26ad970452371411c4a18e
Trey Griffith <trey.griffith@gmail.com>
2013-07-16 11:08:36 -0700

    shell for collection tests

--- test/Collection.test.js
+++ test/Collection.test.js
@@ -1,1 +1,133 @@
 var Collection = require('../lib/Collection');
+
+describe("Mutator Methods", function() {
+
+	it("pops", function() {
+
+	});
+
+	it("pushes", function() {
+
+	});
+
+	it("reverses", function() {
+
+	});
+
+	it("shifts", function() {
+
+	});
+
+	it("sorts", function() {
+
+	});
+
+	it("splices", function() {
+
+	});
+
+	it("unshifts", function() {
+
+	});
+});
+
+describe("Accessor Methods", function() {
+
+	it("has index properties", function() {
+
+	});
+
+	it("concats", function() {
+
+	});
+
+	it("joins", function() {
+
+	});
+
+	it("slices", function() {
+
+	});
+
+	it("converts to a string", function() {
+
+	});
+
+	it("gives the index of an element", function() {
+
+	});
+
+	it("gives the last index of an element", function() {
+
+	});
+});
+
+describe("Iteration Methods", function() {
+
+	it("runs a forEach loop", function() {
+
+	});
+
+	it("every's", function() {
+
+	});
+
+	it("some's", function() {
+
+	});
+
+	it("filters", function() {
+
+	});
+
+	it("maps", function() {
+
+	});
+
+	it("reduces", function() {
+
+	});
+
+	it("reduces right", function() {
+
+	});
+});
+
+describe("Events", function() {
+
+	it("applies events to every child model", function() {
+
+	});
+
+	it("removes events from child models", function() {
+
+	});
+
+	it("applies events to every child model once", function() {
+
+	});
+
+	it("applies events to newly added models", function() {
+
+	});
+
+	it("removes events from removed models", function() {
+
+	});
+});
+
+describe("Custom Methods", function() {
+
+	it("empties the array", function() {
+
+	});
+
+	it("replaces the array", function() {
+
+	});
+
+	it("uses a custom comparison function when using indexOf", function() {
+
+	});
+
+});
